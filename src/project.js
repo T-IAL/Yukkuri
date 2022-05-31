6cc64e72b0418d72091d8d914b723a156c6a7853
varACundefined <70007865+T-IAL@users.noreply.github.com>
2022-05-31 14:02:09 +0800

    Add files via upload

--- src/project.js
+++ src/project.js
@@ -1198,7 +1198,7 @@ window.__require = function e(t, n, o) {
         },
           // 生成水果
           t.prototype.createOneFruit = function (e) {
-            var t = this, n = cc.instantiate(this.fruitPre);
+            var t = this, n = cc.instantiate(this.fruitPre),e=0;
             n.parent = this.lineNode;
             n.getComponent(cc.Sprite).spriteFrame = d.default.Instance.fruit[e];
             n.children[0].getComponent(cc.Sprite).spriteFrame = d.default.Instance.fruit[e];
